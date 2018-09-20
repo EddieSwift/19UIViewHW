cf5d409359160ba3fc3a552a4c9daa9d5c7186a7
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-09-20 09:10:51 +0300

    Initial Commit

---
+++ 19UIViewHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  19UIViewHW
+//
+//  Created by Eduard Galchenko on 9/20/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
